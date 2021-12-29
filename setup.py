53d58e6a0ac4ffd83577504884cdf03761720fc8
IRACK <81044362+IRACK000@users.noreply.github.com>
2021-12-29 12:23:21 +0900

    cx_Freeze용 setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from cx_Freeze import setup, Executable
+
+# ADD FILES
+files = ['icon.ico','gui/','settings.json']
+
+# TARGET
+target = Executable(
+    script="main.pyw",
+    base="Win32GUI",
+    icon="icon.ico"
+)
+
+# SETUP CX FREEZE
+setup(
+    name = "PyOneDark",
+    version = "1.0",
+    description = "Modern GUI for Python applications",
+    author = "Wanderson M. Pimenta",
+    options = {'build_exe' : {'include_files' : files}},
+    executables = [target]
+)
