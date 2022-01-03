d323fbf0b1032c354253333803ca11113b26d087
IRACK <81044362+IRACK000@users.noreply.github.com>
2022-01-03 20:17:26 +0900

    Update build.py

--- build.py
+++ build.py
@@ -17,7 +17,8 @@ if __name__ == "__main__":
     version = input("1. 컴퓨터 기본 파이썬 버전(python)\n2. 파이썬 3.8.10(python3.8, Windows 7용)\n빌드에 사용할 파이썬 버전을 선택하세요. : ")
     py = "python"
     if version == "1":
-        py += ("3" if input("\n1. python\n2. python3\n환경변수에 등록된 python3의 호출 키워드를 선택하세요. : ") == "2" else "")
+        opt = input("\n1. python\n2. python3\n3. 직접 입력\n환경변수에 등록된 python3의 호출 키워드를 선택하세요. : ")
+        py += ("3" if opt == "2" else "" if opt == 1 else input("버전을 입력하세요(python3.?) : ").replace("python", ""))
     elif version == "2":
         py += "3.8"
         print("\n\nWindows 7 호환성을 위해 Pyside버전을 2버전으로 변경합니다. 스크립트가 정상 종료되지 못한 경우 수동으로 qt_core.py 파일을 원래대로 되돌려주세요.\n\n")
