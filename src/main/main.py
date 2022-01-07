e22c936d4e2fdd6addcda56a99cd282ef4ceed92
IRACK <81044362+IRACK000@users.noreply.github.com>
2022-01-07 13:11:27 +0900

    아이콘 경로 수정

--- src/main/main.py
+++ src/main/main.py
@@ -231,7 +231,7 @@ if __name__ == "__main__":
     # APPLICATION
     # ///////////////////////////////////////////////////////////////
     app = QApplication(sys.argv)
-    app.setWindowIcon(QIcon("../../icon.ico"))
+    app.setWindowIcon(QIcon("icon.ico"))
     window = MainWindow()
 
     # EXEC APP
